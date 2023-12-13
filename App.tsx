25cb9ae8fd5e768aabc5724fcf2878a7fa0fa406
KotaSC <is0302ke@gmail.com>
2023-12-13 20:08:54 +0900

    feat: add tab menu

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { Home } from './src/screens/home';
+import { Notification } from './src/screens/notification';
+import { Discover } from './src/screens/discover';
+import { Profile } from './src/screens/profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,6 +24,44 @@ export default function App() {
             tabBarLabel: 'Home',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Notification'
+          component={Notification}
+          options={{
+            tabBarLabel: 'Notification',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name='bell' color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Discover'
+          component={Discover}
+          options={{
+            tabBarLabel: 'Discover',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='compass'
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Profile'
+          component={Profile}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='account'
+                color={color}
+                size={size}
+              />
             ),
           }}
         />
