0f14d0f7483fb0a7fa6df887613072b03f8ea031
KotaSC <is0302ke@gmail.com>
2024-03-14 21:35:26 +0900

    update: Home screen UI

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Card, List, Text, Searchbar, MD3Colors } from "react-native-paper";
+
+import { Layout } from "../components/Layout";
+
+const items = [
+  { title: "Issue", icon: "alpha-i-circle", color: MD3Colors.primary50 },
+  { title: "Pull Request", icon: "source-pull", color: MD3Colors.primary50 },
+  {
+    title: "Discussion",
+    icon: "chat",
+    color: MD3Colors.primary50,
+  },
+];
 
 export const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Layout>
+      <Searchbar
+        placeholder="Search GitHub"
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+      />
+      <Text style={styles.titleContainer}>自分の作業</Text>
+      <Card>
+        <Card.Content>
+          <List.Section>
+            {items.map((item) => (
+              <List.Item
+                title={item.title}
+                left={() => <List.Icon color={item.color} icon={item.icon} />}
+              />
+            ))}
+          </List.Section>
+        </Card.Content>
+      </Card>
+    </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
